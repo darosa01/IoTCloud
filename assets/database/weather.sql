80f4b16a6a97a396575bb6f7b64df3cbdb5d5f24
David Romero <57216626+darosa01@users.noreply.github.com>
2022-11-26 14:44:59 +0100

    Database format uploaded

--- assets/database/weather.sql
+++ assets/database/weather.sql
@@ -1,1 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-11-2022 a las 14:37:50
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.11
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `weather`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `air`
+--
+
+CREATE TABLE `air` (
+  `id` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `PM10` int(11) NOT NULL,
+  `O3` int(11) NOT NULL,
+  `NO2` int(11) NOT NULL,
+  `SO2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `humidity`
+--
+
+CREATE TABLE `humidity` (
+  `id` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `value` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `temperature`
+--
+
+CREATE TABLE `temperature` (
+  `id` int(11) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `value` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `air`
+--
+ALTER TABLE `air`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `humidity`
+--
+ALTER TABLE `humidity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `temperature`
+--
+ALTER TABLE `temperature`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `air`
+--
+ALTER TABLE `air`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `humidity`
+--
+ALTER TABLE `humidity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `temperature`
+--
+ALTER TABLE `temperature`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
