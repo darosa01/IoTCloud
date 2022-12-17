3ef50d06774539fe6a977290c24273b09b20789b
David Romero <57216626+darosa01@users.noreply.github.com>
2022-12-17 13:04:14 +0100

    Database format correction

--- assets/database/weather.sql
+++ assets/database/weather.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 26-11-2022 a las 14:37:50
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.11
+-- Servidor: PMYSQL126.dns-servicio.com:3306
+-- Tiempo de generación: 16-12-2022 a las 19:52:14
+-- Versión del servidor: 5.7.40
+-- Versión de PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,35 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `weather`
+-- Base de datos: `7439983_weather`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `air`
---
-
-CREATE TABLE `air` (
-  `id` int(11) NOT NULL,
-  `datetime` datetime NOT NULL,
-  `PM10` int(11) NOT NULL,
-  `O3` int(11) NOT NULL,
-  `NO2` int(11) NOT NULL,
-  `SO2` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `humidity`
---
-
-CREATE TABLE `humidity` (
-  `id` int(11) NOT NULL,
-  `datetime` datetime NOT NULL,
-  `value` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -56,25 +29,13 @@ CREATE TABLE `humidity` (
 
 CREATE TABLE `temperature` (
   `id` int(11) NOT NULL,
-  `datetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `value` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `air`
---
-ALTER TABLE `air`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `humidity`
---
-ALTER TABLE `humidity`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `temperature`
@@ -85,18 +46,6 @@ ALTER TABLE `temperature`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `air`
---
-ALTER TABLE `air`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `humidity`
---
-ALTER TABLE `humidity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `temperature`
